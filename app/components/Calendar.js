32a284d0b18909e826602b5279500e6242892ba2
John Hilts <johnhilts@live.com>
2016-07-13 23:07:10 -0700

    Refined the algorithm but still of by ONE cell!
    very close though and like how things are coming along so far

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router'
 
-const showDate = (date) => {
+const showDate = (monthInfo, cellIndex) => {
+  console.log("cell: " + cellIndex);
+  console.log("day: " + monthInfo.day);
   return (
-    <td key={date} style={{width: 100, textAlign: 'center', borderStyle: 'solid', borderWidth: 5,
+    monthInfo.isEmpty
+    ?
+    <td key={cellIndex} style={{width: 100, borderStyle: 'dotted', borderWidth: 1,
       paddingLeft: 25, paddingRight: 25, paddingTop: 25, paddingBottom: 25, }}>
-      <Link to={{pathname: 'calendar/' + date, state: {test: 'abc', }}}>{date}</Link>
+      &nbsp;
+    </td>
+    :
+    <td key={cellIndex} style={{width: 100, textAlign: 'center', borderStyle: 'solid', borderWidth: 5,
+      paddingLeft: 25, paddingRight: 25, paddingTop: 25, paddingBottom: 25, }}>
+      <Link to={{pathname: 'calendar/' + monthInfo.day, state: {test: 'abc', }}}>{monthInfo.day}</Link>
     </td>
   )
 }
@@ -14,21 +23,16 @@ export default function Calendar(props) {
   return (
     <table cellPadding="10" cellSpacing="10" border="5">
       <tbody>
-        <tr>
-          {props.monthDates.filter((date)=>{return date <= 7;}).map(showDate)}
-        </tr>
-        <tr>
-          {props.monthDates.filter((date)=>{return date > 7 && date <= 14;}).map(showDate)}
-        </tr>
-        <tr>
-          {props.monthDates.filter((date)=>{return date > 14 && date <= 21;}).map(showDate)}
-        </tr>
-        <tr>
-          {props.monthDates.filter((date)=>{return date > 21 && date <= 28;}).map(showDate)}
-        </tr>
-        <tr>
-          {props.monthDates.filter((date)=>{return date > 28 && date <= props.monthDates.length;}).map(showDate)}
-        </tr>
+        {[1, 2, 3, 4, 5, 6].map(weekIndex => {
+          let startCell = (weekIndex - 1) * 7;
+          let endCell = startCell + 7;
+          return (
+            <tr>
+              {props.monthGrid.filter((monthInfo, cellIndex)=>{return cellIndex > startCell && cellIndex <= endCell;}).map(showDate)}
+            </tr>
+          )
+        }
+        )}
       </tbody>
     </table>
   )
--- app/containers/CalendarContainer.js
+++ app/containers/CalendarContainer.js
@@ -10,16 +10,29 @@ const CalendarContainer = React.createClass({
     )
   },
 
-  buildMonthDates() {
+  buildMonthGrid() {
     let year = moment().year();
     let month = moment().month();
     let theMonth = moment(new Date(year, month, 1));
     let daysInMonth = theMonth.daysInMonth();
-    let monthDates = [];
-    for (let i = 1; i <= daysInMonth; i++) {
-      monthDates.push(i);
+    let monthGrid = [];
+    let nextCellIndex = 0;
+    for (let day = 1; day <= daysInMonth; day++) {
+      let date = new moment(new Date(year, month, day));
+      for (let cellIndex = nextCellIndex; cellIndex <= 6; cellIndex++) {
+        if (date.weekday() == cellIndex) {
+          let monthInfo = {year: date.year(), month: date.month(), day: date.date(), isEmpty: false, };
+          monthGrid.push(monthInfo);
+          nextCellIndex = cellIndex < 6 ? cellIndex + 1 : 0;
+          break;
+        }
+        else {
+          let emptyInfo = {isEmpty: true, };
+          monthGrid.push(emptyInfo);
+        }
+      }
     }
-    return monthDates;
+    return monthGrid;
   },
 
   render() {
@@ -28,7 +41,7 @@ const CalendarContainer = React.createClass({
     return (
       <div>
         <h2>{previousMonthLink} {moment().format('MMMM')} {nextMonthLink}</h2>
-        <Calendar monthDates={this.buildMonthDates()} />
+        <Calendar monthGrid={this.buildMonthGrid()} />
       </div>
     )
   }
