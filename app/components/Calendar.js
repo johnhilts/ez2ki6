3b91c9dc0d576ed3a44b9a086fbf3736c5e9b3ed
John Hilts <johnhilts@live.com>
2016-12-03 22:31:39 -0800

    refactored for better readability

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -8,27 +8,38 @@ const weekdayHeader = (weekdayName) => {
   )
 }
 
-// NOTE: I couldn't use the index parameter because what's being mapped is a *filtered* array, not the complete original
-// so, to get around it I had to add the index to the object so that I have the original array's index even after going through a filter
-const showDate = (monthInfo) => {
-  let dayStyle = monthInfo.hasData
+const getDecorator = (monthInfo) => {
+  let decorator = {
+    dayStyle : monthInfo.hasData
     ? {fontWeight: 'bold', fontSize: 'larger', }
     : {fontWeight: 'normal', }
-  let dataIcon = monthInfo.hasData
+    ,
+    dataIcon : monthInfo.hasData
     ? <span className='glyphicon glyphicon-list-alt'></span>
     : ''
-  if (monthInfo.isToday) {
-    dayStyle.color = '#EC7063';
-    dayStyle.border = '2px solid red';
+    ,
   }
+  if (monthInfo.isToday) {
+    decorator.dayStyle.color = '#EC7063';
+    decorator.dayStyle.border = '2px solid red';
+  }
+
+  return decorator;
+}
+
+const renderEmptyCell = (monthInfo) => {
   return (
-    monthInfo.isEmpty
-    ?
     <td key={monthInfo.absoluteIndex} style={{width: 100, borderStyle: 'dotted', borderWidth: 1,
       paddingLeft: 25, paddingRight: 25, paddingTop: 25, paddingBottom: 25, }}>
       &nbsp;
     </td>
-    :
+  )
+}
+
+const renderDateCell = (monthInfo) => {
+  let {dayStyle, dataIcon} = getDecorator(monthInfo);
+
+  return (
     <td key={monthInfo.absoluteIndex} style={{width: 100, textAlign: 'center', borderStyle: 'solid', borderWidth: 5,
       paddingLeft: 25, paddingRight: 25, paddingTop: 25, paddingBottom: 25, }}>
       <Link style={dayStyle} to={{pathname: 'day', state: {monthInfo: monthInfo, }}}>
@@ -39,7 +50,29 @@ const showDate = (monthInfo) => {
   )
 }
 
+// NOTE: I couldn't use the index parameter because what's being mapped is a *filtered* array, not the complete original
+// so, to get around it I had to add the index to the object so that I have the original array's index even after going through a filter
+const showDate = (monthInfo) => {
+  return monthInfo.isEmpty ? renderEmptyCell(monthInfo) : renderDateCell(monthInfo);
+}
+
 export default function Calendar(props) {
+
+  const weekIterator = (weekIndex) => {
+    let startCell = (weekIndex - 1) * 7;
+    let endCell = startCell + 7;
+
+    const currentWeek = (monthInfo, cellIndex) => {
+      return cellIndex >= startCell && cellIndex < endCell;
+    }
+
+    return (
+      <tr key={weekIndex}>
+        {props.monthGrid.filter(currentWeek).map(showDate)}
+      </tr>
+    )
+  }
+
   return (
     <table cellPadding="10" cellSpacing="10" border="5">
       <thead>
@@ -48,16 +81,7 @@ export default function Calendar(props) {
         </tr>
       </thead>
       <tbody>
-        {[1, 2, 3, 4, 5, 6].map(weekIndex => {
-          let startCell = (weekIndex - 1) * 7;
-          let endCell = startCell + 7;
-          return (
-            <tr key={weekIndex}>
-              {props.monthGrid.filter((monthInfo, cellIndex)=>{return cellIndex >= startCell && cellIndex < endCell;}).map(showDate)}
-            </tr>
-          )
-        }
-        )}
+        {[1, 2, 3, 4, 5, 6].map(weekIterator)}
       </tbody>
     </table>
   )
