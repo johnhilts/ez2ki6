23f09e7f5b967a6cc96ef59d2f1cf1179e069b31
John Hilts <johnhilts@live.com>
2016-12-31 01:21:29 -0800

    Changed test syntax
    looks more like the rspec syntax I've seen on DestroyAllSoftware ...

--- __tests__/util/stringutils.spec.js
+++ __tests__/util/stringutils.spec.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import * as stringUtils from '../../app/util/stringutils';
 
-test('replaces line breaks with HTML <br> tags', () => {
-  let formattedHtml =
-    [
-			<span key="0">
-				line1
-				<br/>
-			</span>
-      ,
-			<span key="1">
-				line2
-				<br/>
-			</span>
-  ]
+describe('stringUtils', () => {
+  it('replaces line breaks with HTML <br> tags', () => {
+    let formattedHtml =
+      [
+  			<span key="0">
+  				line1
+  				<br/>
+  			</span>
+        ,
+  			<span key="1">
+  				line2
+  				<br/>
+  			</span>
+    ]
 
-  expect(stringUtils.formatLineBreaksForHtml('line1\nline2')).toEqual(formattedHtml);
-});
+    expect(stringUtils.formatLineBreaksForHtml('line1\nline2')).toEqual(formattedHtml);
+  });
+})
