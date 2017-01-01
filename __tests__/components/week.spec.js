30a1e23c1c9396f436bec09b76aa1ed09b56e8ec
John Hilts <johnhilts@live.com>
2016-12-31 16:35:00 -0800

    Got *real* snapshot test to pass
    I needed to install the following:
    npm i object-assign
    npm i fbjs
    They are dependencies for react-test-render
    No changes to package.json

--- __tests__/components/week.spec.js
+++ __tests__/components/week.spec.js
@@ -1,6 +1,19 @@
+import React, { Component } from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+import Week from '../../app/components/Week';
+
+const getWeekInfo = () => {
+  return [{year: 2016, month: 11, day: 15, absoluteIndex: 0, isToday: false, isCurrentMonth: true, hasData: false, }];
+  // monthInfo.dateEntries = this.state.dates.filter(dbDate => {return dbDate.year == date.year && dbDate.month == date.month && dbDate.day == date.day;});
+}
+
 describe('Week component snapshot test', () => {
   it('Should compare the Week component with a snapshot', () => {
-    const component = '<div>Hello Jest</div>'
-    expect(component).toMatchSnapshot();
+    let weekInfo = getWeekInfo();
+    const component = ReactTestRenderer.create(
+      <Week weekInfo={weekInfo} />
+    );
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
   })
 });
