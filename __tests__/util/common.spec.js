8b21e4a086b97c9fcb4722a5abce42baf06aee43
John Hilts <johnhilts@live.com>
2017-01-11 10:48:52 -0800

    restored jest tests
    configured jest with --watchAll in project.json

---
+++ __tests__/util/common.spec.js
@@ -0,0 +1,19 @@
+import * as common from '../../app/util/common';
+
+describe('range', () => {
+  it('returns a range of 0..1', () => {
+    let expected = [0, 1];
+    let actual = common.range(0, 2);
+    expect(expected).toEqual(actual);
+  });
+  it('returns a range of 1..3', () => {
+    let expected = [1, 2, 3];
+    let actual = common.range(1, 4);
+    expect(expected).toEqual(actual);
+  });
+  it('returns a range of 0..2 with only 1 parameter', () => {
+    let expected = [0, 1, 2];
+    let actual = common.range(3);
+    expect(expected).toEqual(actual);
+  });
+})
---
+++ __tests__/util/dateutils.spec.js
@@ -0,0 +1,23 @@
+import * as dateUtils from '../../app/util/dateutils';
+
+const getFormattedYearMonthByYearMonthAndOffset = (currentYearMonth, formattedYearMonth, offset) => {
+    expect(dateUtils.getFormattedYearMonthByYearMonthAndOffset(currentYearMonth, offset)).toBe(formattedYearMonth);
+}
+
+describe('dateUtils', () => {
+  it('gets a formatted year/month for next month', () => {
+    expect(getFormattedYearMonthByYearMonthAndOffset('201611', '201612', dateUtils.monthOffset.next));
+  });
+  it('gets a formatted year/month for previous month', () => {
+    expect(getFormattedYearMonthByYearMonthAndOffset('201611', '201610', dateUtils.monthOffset.previous));
+  });
+  it('gets a formatted year/month for this month', () => {
+    expect(getFormattedYearMonthByYearMonthAndOffset('201611', '201611', dateUtils.monthOffset.current));
+  });
+  it('parses Formatted Year Month', () => {
+    let formattedYearMonth = '201701';
+    let expected = {year: 2017, month: 0, daysInMonth: 31, };
+    let actual = dateUtils.getYearMonthByFormattedYearMonth(formattedYearMonth);
+    expect(actual).toEqual(expected);
+  });
+})
