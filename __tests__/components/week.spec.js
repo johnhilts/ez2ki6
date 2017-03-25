90ce8e67af1365f28d3e2556834cccd01d1c45bc
John Hilts <johnhilts@live.com>
2017-03-25 16:59:08 -0700

    updated week test spec

--- __tests__/components/week.spec.js
+++ __tests__/components/week.spec.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import Week from '../../app/components/Week';
+import * as calendarGrid from '../../app/core/calendarGrid';
+import * as testHelper from '../../test/Helper/DateHelper';
 
 const getWeekInfo = () => {
   return [{year: 2016, month: 11, day: 15, absoluteIndex: 0, isToday: false, isCurrentMonth: true, hasData: false, }];
@@ -11,7 +13,7 @@ describe('Week component snapshot test', () => {
   it('Should compare the Week component with a snapshot', () => {
     let weekInfo = getWeekInfo();
     const component = ReactTestRenderer.create(
-      <Week weekInfo={weekInfo} />
+      <Week weekInfo={weekInfo} weekIndex={2} monthGrid={calendarGrid.buildMonthGrid(testHelper.dates, testHelper.currentYearMonth)} />
     );
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
