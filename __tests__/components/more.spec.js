30841c7b64716b326f1c9ce96a71fcef1c0733c3
John Hilts <johnhilts@live.com>
2017-06-01 00:12:54 -0700

    updated more component test spec

--- __tests__/components/more.spec.js
+++ __tests__/components/more.spec.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import More from '../../app/components/More';
-/*
-import * as enums from '../../app/core/enums';
-import * as calendarGrid from '../../app/core/calendarGrid';
-import * as testHelper from '../../test/Helper/DateHelper';
-*/
 import * as dateUtils from '../../app/util/dateutils';
 
 describe('More component snapshot test', () => {
@@ -13,14 +8,15 @@ describe('More component snapshot test', () => {
   it('Should compare the More component with a snapshot', () => {
     // TODO - consider making MoreContainer.searchFields publicly accessible
     const searchFields = {searhText: 0, fromDateMonth: 1, fromDateDay: 2, fromDateYear: 3, toDateMonth: 4, toDateDay: 5, toDateYear: 6, };
+    const testDate = {year: 2017, month: 4, day: 31, }
     const component = ReactTestRenderer.create(
       <More
         searchResults={[]}
-        fromDate={dateUtils.getCurrentDate()}
-        toDate={dateUtils.getCurrentDate()}
+        fromDate={testDate}
+        toDate={testDate}
         searchFields={searchFields}
         onDateChange={()=>{}}
-        years={[dateUtils.getCurrentDate().year]}
+        years={[2017]}
         user={{themeId: 0}}
       />
     );
