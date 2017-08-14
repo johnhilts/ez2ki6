0ef99ba28f3db3284351fa033272f63adf1d8450
John Hilts <johnhilts@live.com>
2017-08-13 19:00:42 -0700

    Applied dark theme to login and registration
    I had missed those 2 views originally, so that's why this commit is so
    late

--- app/components/Login.js
+++ app/components/Login.js
@@ -3,9 +3,9 @@ import React from 'react';
  export default function Login(props) {
   return (
     <form onSubmit={props.onSubmit}>
-        <input value={props.email} onChange={props.onUpdateEmail} placeholder="email" type="email" /><br />
+        <input value={props.email} onChange={props.onUpdateEmail} placeholder="email" type="email" className="invertedText" /><br />
         <br />
-        <input type="password" value={props.password} onChange={props.onUpdatePassword} placeholder="password" />
+        <input type="password" value={props.password} onChange={props.onUpdatePassword} placeholder="password" className="invertedText" />
         <br />
         <br />
         <input type="submit" value="Login" className="btn btn-info" />
--- app/components/Register.js
+++ app/components/Register.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Register(props) {
   return (
     <form onSubmit={props.onSubmit}>
-        <input value={props.userName} onChange={props.onUpdateUserName} placeholder="your name" /><br />
+        <input value={props.userName} onChange={props.onUpdateUserName} placeholder="your name" className="invertedText" /><br />
         <br />
-        <input value={props.email} onChange={props.onUpdateEmail} placeholder="email" type="email" /><br />
+        <input value={props.email} onChange={props.onUpdateEmail} placeholder="email" type="email" className="invertedText" /><br />
         <br />
-        <input type="password" value={props.password} onChange={props.onUpdatePassword} placeholder="password" />
+        <input type="password" value={props.password} onChange={props.onUpdatePassword} placeholder="password" className="invertedText" />
         <br />
         <br />
         <input type="submit" value="Register" className="btn btn-info" />
